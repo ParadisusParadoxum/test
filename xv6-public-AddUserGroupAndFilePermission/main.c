765c3e59eb3d33037d8a7f543121996809e7ede6
ParadisusParadoxum <cek2llm@gmail.com>
2018-12-27 22:35:10 +0800

    Add files via upload

--- xv6-public-AddUserGroupAndFilePermission/main.c
+++ xv6-public-AddUserGroupAndFilePermission/main.c
@@ -35,7 +35,6 @@ main(void)
   kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
   userinit();      // first user process
   mpmain();        // finish this processor's setup
-  login();         //login
 }
 
 // Other CPUs jump here from entryother.S.
@@ -94,13 +93,6 @@ startothers(void)
       ;
   }
 }
-
-static void
-login(void)
-{
-  printf("Hello");
-}
-
 // The boot page table used in entry.S and entryother.S.
 // Page directories (and page tables) must start on page boundaries,
 // hence the __aligned__ attribute.
